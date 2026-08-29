4edd9b515a3830bdc15bc6a1d8420ccae039d98f
Joni Lappalainen <joni.lappalainen@proton.me>
2026-08-29 13:18:28 +0300

    Make encryption optional

--- backup.sh
+++ backup.sh
@@ -1,42 +1,47 @@
 #!/usr/bin/env bash
 
-set -e
-
 SCRIPT_DIR=$(dirname "$0")
 ENVFILE=${1:-$SCRIPT_DIR/.env}
 source $ENVFILE
+is_mounted=false
 
-# mount
-GOCRYPTFS="$ENCRYPTED_DIR/gocryptfs.conf"
-if [ ! -f "$GOCRYPTFS" ]; then
-  echo "Invalid ENCRYPTED_DIR $ENCRYPTED_DIR"
-  exit 1
-fi
-
-if [ ! -d "$MOUNT_DIR" ]; then
-  echo "Invalid MOUNT_DIR $MOUNT_DIR"
-  exit 1
-fi
-
-if ! mountpoint -q "$MOUNT_DIR"; then
-  gocryptfs $ENCRYPTED_DIR $MOUNT_DIR
+if [ "$USE_GOCRYPT" = "true" ]; then
+  # mount
+  GOCRYPTFS="$ENCRYPTED_DIR/gocryptfs.conf"
+  if [ ! -f "$GOCRYPTFS" ]; then
+    echo "Invalid ENCRYPTED_DIR $ENCRYPTED_DIR"
+    exit 1
+  fi
+  if [ ! -d "$MOUNT_DIR" ]; then
+    echo "Invalid MOUNT_DIR $MOUNT_DIR"
+    exit 1
+  fi
+  if ! mountpoint -q "$MOUNT_DIR"; then
+    gocryptfs $ENCRYPTED_DIR $MOUNT_DIR
+  else
+    echo "Already mounted at $MOUNT_DIR"
+  fi
+  is_mounted=true
 else
-  echo "Already mounted at $MOUNT_DIR"
+  is_mounted=true
 fi
 
+echo "is_mounted $is_mounted"
 # backup
-if [ -d "$MOUNT_DIR" ]; then
+if [ $is_mounted = "true" ] && [ -d "$MOUNT_DIR" ]; then
   # --archive, -a            archive mode is -rlptgoD (no -A,-X,-U,-N,-H)
   # --verbose, -v            increase verbosity
   # --compress, -z           compress file data during the transfer
   # --recursive, -r          recurse into directories
   # --copy-links, -L         transform symlink into referent file/directory
   # --perms, -p              preserve permissions
-  rsync -avzrLp --delete --delete-excluded --files-from="$INCLUDES_FILE" --exclude-from="$EXCLUDES_FILE" $SOURCE_DIR $MOUNT_DIR
-
+  rsync -avzrLp --copy-links --safe-links --delete --delete-excluded --files-from="$INCLUDES_FILE" --exclude-from="$EXCLUDES_FILE" $SOURCE_DIR $MOUNT_DIR
 else
   echo "invalid target/mount dir $MOUNT_DIR"
 fi
 
-# unmount
-fusermount -u $MOUNT_DIR
+if [ "$USE_GOCRYPT" = "true" ] && [ $is_mounted = "true" ]; then
+  # unmount
+  echo "umounting $MOUNT_DIR"
+  fusermount -u $MOUNT_DIR
+fi
--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
-#!/bin/env
+#!/usr/bin/env bash
+
+set -e
 
 SCRIPT_DIR=$(dirname "$0")
 ENVFILE=${1:-$SCRIPT_DIR/.env}
 source $ENVFILE
 
+echo "Initializing '$ENCRYPTED_DIR'"
 gocryptfs -init $ENCRYPTED_DIR
